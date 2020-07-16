f28beff3a288321de63ceb4ad936a14edc0ba68b
Ihar Suvorau <ihar.suvorau@gmail.com>
2020-07-16 18:14:16 +0300

    additional motions added, asyn say and move tasks execution implemented

--- app/src/main/java/ee/ims/tomato/MainActivity.java
+++ app/src/main/java/ee/ims/tomato/MainActivity.java
@@ -1,5 +1,6 @@
 package ee.ims.tomato;
 
+import android.content.res.Resources;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -19,11 +20,17 @@ import com.aldebaran.qi.sdk.object.actuation.Animation;
 import com.aldebaran.qi.sdk.object.conversation.Say;
 
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Field;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringJoiner;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -38,11 +45,20 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
     TextView outputText;
 
     PepperTask task = new PepperTask();
+    PepperTask currentSayTask = new PepperTask();
+    PepperTask currentMoveTask = new PepperTask();
     Boolean isNewTask = false;
+    Boolean isNewSayTask = false;
+    Boolean isNewMoveTask = false;
+    String packageName;
+    String[] allRawResources;
+    String allRawResourcesString;
+    WebSocket webSocket;
 
     private static class PepperTask {
         public String command;
         public String content;
+        public String name;
         public Integer delay;
     }
 
@@ -54,8 +70,26 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
         // Pepper initiation
         QiSDK.register(this, this);
 
+        // some fields for a basic feedback
         outputText = findViewById(R.id.outputText);
         outputText.setText("Output goes here.");
+
+        // getting all motion files to send it to the web server for a user to use
+        Field[] fields = R.raw.class.getFields();
+        String[] allRawResources = new String[fields.length];
+        for (int i = 0; i < fields.length; i++) {
+            allRawResources[i] = fields[i].getName();
+        }
+        Log.d("onCreate", String.format("found resources: %d, first of them: %s", allRawResources.length, allRawResources[0]));
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("moves", new JSONArray(allRawResources));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        allRawResourcesString = obj.toString();
+
+        packageName = getPackageName();
     }
 
     // Pepper RobotLifecycleCallbacks implementation
@@ -65,43 +99,68 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
         Log.i("PepperCallbacks", "focus gained");
         this.qiContext = qiContext;
 
-        // Establishing a connection with the server with Pepper tasks
-        String serverURL = "ws://10.0.2.2:8080/pepper/initiate";
-//        String serverURL = "ws://192.168.1.227:8080/pepper/initiate";
-        Request request = new Request.Builder().url(serverURL).build();
-        PepperTasksListener listener = new PepperTasksListener();
-        OkHttpClient client = new OkHttpClient();
-        WebSocket webSocket = client.newWebSocket(request, listener);
+        establishConnection();
 
+        // main loop for the robot control
         for (; ; ) {
-            if (isNewTask) {
-                if (task.command.equals("say")) {
-                    runOnUiThread(() -> outputText.setText(task.command + ": " + task.content));
+            if (isNewSayTask) {
+                runOnUiThread(() -> outputText.setText(currentSayTask.command + ": " + currentSayTask.content));
 
-                    Say say = SayBuilder.with(qiContext).withText(task.content).build();
-                    say.run();
+                Say say = SayBuilder.with(qiContext).withText(currentSayTask.content).build();
+                say.async().run().thenConsume(voidFuture -> {
+                    if (voidFuture.isSuccess()) {
+                        Log.d("onRobotFocusGained", "say action has finished");
+                    } else if (voidFuture.hasError()) {
+                        Log.d("onRobotFocusGained", "say action has the error:", voidFuture.getError());
+                    }
+                });
+                isNewSayTask = false;
+            }
+
+            if (isNewMoveTask) {
+                runOnUiThread(() -> outputText.setText(currentMoveTask.command));
+
+                Log.d("onRobotFocusGained", "task move");
+
+                Animation motion = null;
+
+                if (currentMoveTask.content.length() == 0) {
+                    Log.d("onRobotFocusGained", "empty content");
+                    int resID = getResources().getIdentifier(currentMoveTask.name, "raw", packageName);
+                    if (resID != 0) {
+                        motion = AnimationBuilder.with(qiContext).withResources(resID).build();
+                    } else {
+                        Log.d("onRobotFocusGained", String.format("can't find resource %s", currentMoveTask.name));
+                    }
+                } else {
+                    Log.d("onRobotFocusGained", "content is present");
+                    motion = AnimationBuilder.with(qiContext).withTexts(currentMoveTask.content).build();
                 }
 
-                if (task.command.equals("move")) {
-                    runOnUiThread(() -> outputText.setText(task.command));
+                if (motion != null) {
+                    Animate move = AnimateBuilder.with(qiContext).withAnimation(motion).build();
+                    Log.d("onRobotFocusGained", "animate is ready");
 
-                    if (task.delay > 0) {
+                    if (currentMoveTask.delay > 0) {
+                        Log.d("onRobotFocusGained", "delay is needed");
                         try {
-                            Thread.sleep(task.delay);
-                            Animation motion = AnimationBuilder.with(qiContext).withTexts(task.content).build();
-                            Animate move = AnimateBuilder.with(qiContext).withAnimation(motion).build();
-                            move.run();
+                            Thread.sleep(currentMoveTask.delay);
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
-                    } else {
-                        Animation motion = AnimationBuilder.with(qiContext).withTexts(task.content).build();
-                        Animate move = AnimateBuilder.with(qiContext).withAnimation(motion).build();
-                        move.run();
                     }
+
+                    Log.d("onRobotFocusGained", "running");
+                    move.async().run().thenConsume(voidFuture -> {
+                        if (voidFuture.isSuccess()) {
+                            Log.d("onRobotFocusGained", "animation has finished");
+                        } else if (voidFuture.hasError()) {
+                            Log.d("onRobotFocusGained", "animation has the error:", voidFuture.getError());
+                        }
+                    });
                 }
 
-                isNewTask = false;
+                isNewMoveTask = false;
             }
         }
     }
@@ -117,6 +176,18 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
         Log.i("PepperCallbacks", "focus refused: " + reason);
     }
 
+    // Helper methods
+
+    private void establishConnection() {
+        // Establishing a connection with the server with Pepper tasks
+//        String serverURL = "ws://10.0.2.2:8080/pepper/initiate";
+        String serverURL = "ws://192.168.1.227:8080/pepper/initiate";
+        Request request = new Request.Builder().url(serverURL).build();
+        PepperTasksListener listener = new PepperTasksListener();
+        OkHttpClient client = new OkHttpClient();
+        webSocket = client.newWebSocket(request, listener);
+    }
+
     // WebSocket implementation with okhttp3
     public class PepperTasksListener extends WebSocketListener {
         private static final int NORMAL_CLOSURE_STATUS = 1000;
@@ -124,6 +195,10 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
         @Override
         public void onOpen(@NotNull WebSocket webSocket, Response response) {
             Log.i("WebSocket", "opened");
+
+            // informing the web server about available resources
+            webSocket.send(allRawResourcesString);
+            Log.d("PepperTasksListener", String.format("resources information sent: %s", allRawResourcesString));
         }
 
         @Override
@@ -138,11 +213,28 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
             if (msgJSON != null) {
                 Log.d("WebSocket", msgJSON.toString());
                 try {
-                    task.command = msgJSON.getString("command");
-                    task.content = msgJSON.getString("content");
-                    task.delay = msgJSON.getInt("delay");
-                    Log.d("WebSocket", String.format("command: %s, delay: %d", task.command, task.delay));
-                    isNewTask = true;
+                    String command = msgJSON.getString("command");
+                    String content = msgJSON.getString("content");
+                    String name = msgJSON.getString("name");
+                    Integer delay = msgJSON.getInt("delay");
+
+                    if (command.equals("say")) {
+                        currentSayTask.command = command;
+                        currentSayTask.content = content;
+                        currentSayTask.name = name;
+                        currentSayTask.delay = delay;
+                        isNewSayTask = true;
+                    }
+                    if (command.equals("move")) {
+                        currentMoveTask.command = command;
+                        currentMoveTask.content = content;
+                        currentMoveTask.name = name;
+                        currentMoveTask.delay = delay;
+                        isNewMoveTask = true;
+                    }
+
+                    Log.d("WebSocket", String.format("command: %s, name: %s, delay: %d",
+                            command, name, delay));
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -161,8 +253,9 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
             if (msgJSON != null) {
                 try {
                     task.command = msgJSON.getString("command");
-
                     task.content = msgJSON.getString("content");
+                    task.name = msgJSON.getString("name");
+                    task.delay = msgJSON.getInt("delay");
                     isNewTask = true;
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -177,9 +270,16 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
         }
 
         @Override
-        public void onFailure(@NotNull WebSocket webSocket, Throwable throwable, Response response) {
-            Log.e("WebSocket", throwable.getMessage());
-            Log.i("WebSocket", response.toString());
+        public void onFailure(WebSocket webSocket, Throwable throwable, Response response) {
+            webSocket.close(NORMAL_CLOSURE_STATUS, null);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Log.d("PepperTasksListener", "re-establishing connection");
+            establishConnection();
         }
     }
 }
